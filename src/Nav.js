c74440f2d1c766cb8299ac9ba574369a44fbd27d
Samarthya2912 <samarthya55@gmail.com>
2021-07-29 11:02:59 +0530

    basic

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.css';
+
+function Nav() {
+    return (
+        <section>
+            <button>All</button>
+            <button>Breakfast</button>
+            <button>Lunch</button>
+            <button>Shakes</button>
+        </section>
+    )
+}
+
+export default Nav;
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import './Title.css';
+
+function Title(){
+    return (
+        <header>
+            <h1>Our Menu</h1>
+            <div className="underline"></div>
+        </header>
+    )
+}
+
+export default Title;
